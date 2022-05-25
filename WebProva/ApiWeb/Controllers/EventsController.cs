224100a6e20c3307ef6b9ea46e1afe2cfe745eae
Iagor M.L <iagorluchesi@gmail.com>
2022-05-24 22:36:33 -0400

    adding post delet put and getbyid controllers

--- WebProva/ApiWeb/Controllers/EventsController.cs
+++ WebProva/ApiWeb/Controllers/EventsController.cs
@@ -2,7 +2,6 @@
 using AppCore.Model;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ApiWeb.Controllers
 {
@@ -23,29 +22,48 @@ namespace ApiWeb.Controllers
             return Ok( _db.Events);
         }
 
-        // GET api/<EventsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetByID(int id)
         {
-            return "value";
+            var events = _db.Events.Find(id);
+            if (events == null)
+                return NotFound();
+            return Ok( events );
+            
         }
 
         // POST api/<EventsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Event events)
         {
+            if (events == null)
+                return BadRequest();
+            _db.Events.Add(events);
+            _db.SaveChanges();
+            return Created($"events/{events.Id}", events);
         }
 
         // PUT api/<EventsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Event events)
         {
+            if(id != events.Id)
+                return BadRequest();
+            _db.Events.Update(events);
+            _db.SaveChanges();
+            return NoContent();
         }
 
         // DELETE api/<EventsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var eventos = _db.Events.Find(id);
+            if (eventos == null)
+                return NotFound();
+            _db.Events.Remove(eventos);
+            _db.SaveChanges();
+            return NoContent();
         }
     }
 }
